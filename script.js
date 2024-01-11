5cbb32f68bc220214629446443dca1007b31abd4
Val Garman <sir.mangocane@gmail.com>
2024-01-10 19:59:03 -0500

    Create a prototype for rps ui

--- script.js
+++ script.js
@@ -1,44 +1,63 @@
+let playerChoice = "";
+let wins = 0;
+let losses = 0;
+
+const displayPlayerScore = document.querySelector("#pScore");
+const displayComputerScore = document.querySelector("#cScore");
+
+function UpdateScore(roundResult) {
+    if (roundResult === "win") {
+        wins += 1;
+    } else if (roundResult === "lose") {
+        losses += 1;
+    }
+
+    displayPlayerScore.innerText = `Player: ${wins}`
+    displayComputerScore.innerText = `Computer: ${losses}`
+}
+
+const choices = document.querySelectorAll("#selectionContainer > button");
+choices.forEach((button) => {
+    button.addEventListener("click", () => {
+        playerChoice = button.id;
+    });
+});
+
+
+const roundButton = document.querySelector("#startRound");
+roundButton.addEventListener("click", () => {
+    if (playerChoice != "") {
+        let computerChoice = getComputerChoice()
+        let result = playRound(playerChoice, getComputerChoice());
+        UpdateScore(result);
+
+        playerChoice = "";
+    }
+});
+
+
 function getComputerChoice() {
     let options = ["rock", "paper", "scissors"];
     return options[Math.floor(Math.random() * 3)];
 }
 
 
-function getPlayerChoice() {
-    let choice = prompt('Choose "rock" "paper" or "scissors"');
-    return choice.toLowerCase();
+function playRound(playerSelection, computerSelection) {  
+    if (playerSelection === "rock") {
+        var winLoseAgainst = ["scissors", "paper"];
+    } else if (playerSelection === "paper") {
+        var winLoseAgainst = ["rock", "scissors"];
+    } else if (playerSelection === "scissors") {
+        var winLoseAgainst = ["paper", "rock"]; 
+    }
+
+    if (computerSelection === winLoseAgainst[0]) {
+        return "win";
+    } else if (computerSelection === winLoseAgainst[1]) {
+        return "lose";
+    } else {
+        return "tie";
+    }        
 }
 
-
-function playRound(playerSelection, computerSelection) {    
-        if (playerSelection === "rock") {
-            if (computerSelection === "scissors") {
-                return "Winner!";
-            } else if (computerSelection === "paper") {
-                return "Loser!";
-            }
-        }
-        if (playerSelection === "paper") {
-            if (computerSelection === "rock") {
-                return "Winner!";
-            } else if (computerSelection === "scissors") {
-                return "Loser!";
-            }
-        }
-        if (playerSelection === "scissors") {
-            if (computerSelection === "paper") {
-                return "Winner!";
-            } else if (computerSelection === "rock") {
-                return "Loser!";
-            }
-        }
-        
-        return "Tie!";
-}
-
-for (let i = 0; i < 5; i++) {
-    let computerChoice = getComputerChoice();
-    let playerChoice = getPlayerChoice();
-
-    console.log(playRound(playerChoice, computerChoice));
-}
+UpdateScore();
